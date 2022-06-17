326a957eed010d738876d829a89497aad9b7e2bb
a279623002 <970979353@qq.com>
2022-06-17 16:42:24 +0800

    add

--- models/base.go
+++ models/base.go
@@ -1,14 +1,23 @@
 package models
 
 import (
+	"fmt"
+	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func init() {
+	dbname := beego.AppConfig.String("dbname")
+	dbhost := beego.AppConfig.String("dbhost")
+	dbuser := beego.AppConfig.String("dbuser")
+	dbpassword := beego.AppConfig.String("dbpassword")
+	dbport := beego.AppConfig.String("dbport")
+	dbprefix := beego.AppConfig.String("dbprefix")
+
 	orm.RegisterDriver("mysql", orm.DRMySQL)
 
-	orm.RegisterDataBase("default", "mysql", "root:shiro2796@/gbb?charset=utf8")
+	orm.RegisterDataBase("default", "mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", dbuser, dbpassword, dbhost, dbport, dbname))
 	// 需要在init中注册定义的model
-	orm.RegisterModelWithPrefix("b_", new(Admin), new(Tag), new(Log), new(Remark), new(System))
+	orm.RegisterModelWithPrefix(dbprefix, new(Admin), new(Tag), new(Log), new(Remark), new(System))
 }
